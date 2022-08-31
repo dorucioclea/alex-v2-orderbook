97b1f278ce799b02266b10010116b7ab01e650b7
fiftyeightandeight <alexd@alexgo.io>
2022-08-31 20:15:26 +0800

    chore: added comments to generate-order-extra-data.ts

--- scripts/generate-order-extra-data.ts
+++ scripts/generate-order-extra-data.ts
@@ -3,6 +3,14 @@
 // These are helper scripts to make development a little bit easier.
 // DO NOT USE REAL SEED PHRASES OR PRIVATE KEYS.
 
+// extra-data is structured as below
+// {
+//    risk: bool, (used by stop limit order. see: https://github.com/alexgo-io/alex-v2-orderbook/blob/ef34bb58e04b6c606ed3f067b76dbf4b3399dafd/contracts/stxdx-exchange-zero.clar#L283-L289 )
+//    stop: uint, (stop-price)
+//    time: uint, (timestamp)
+//    type: uint, (order type. see: https://github.com/alexgo-io/alex-v2-orderbook/blob/ef34bb58e04b6c606ed3f067b76dbf4b3399dafd/contracts/stxdx-exchange-zero.clar#L36-L38)
+// }
+
 if (process.argv.length !== 3) {
   console.log(`Usage: ts-node generate-order-extra-data <extra-data JSON>`);
   process.exit(0);
