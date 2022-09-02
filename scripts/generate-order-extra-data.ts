60d3bf2f61631e18609d7521da51caf6069968e9
fiftyeightandeight <alexd@alexgo.io>
2022-09-02 09:34:11 +0800

    refactor: serialize extra-data uses stacks.js

--- scripts/generate-order-extra-data.ts
+++ scripts/generate-order-extra-data.ts
@@ -11,6 +11,14 @@
 //    type: uint, (order type. see: https://github.com/alexgo-io/alex-v2-orderbook/blob/ef34bb58e04b6c606ed3f067b76dbf4b3399dafd/contracts/stxdx-exchange-zero.clar#L36-L38)
 // }
 
+import {
+  bufferCV,
+  falseCV,
+  serializeCV,
+  trueCV,
+  uintCV,
+} from '@stacks/transactions';
+
 if (process.argv.length !== 3) {
   console.log(`Usage: ts-node generate-order-extra-data <extra-data JSON>`);
   process.exit(0);
@@ -23,39 +31,20 @@ function toBuffer(input: string): Buffer {
   );
 }
 
-function serialise(input: any): Buffer {
-  if (typeof input === 'boolean') {
-    return toBuffer(input === true ? '0x03' : '0x00');
-  } else if (typeof input === 'string') {
-    return Buffer.concat([toBuffer('0x04'), Buffer.from(input, 'ascii')]);
-  } else if (typeof input === 'number') {
-    if (input < 0n) throw new Error('Input cannot be negative');
-    const rs = Buffer.alloc(16);
-    const v = new DataView(rs.buffer, rs.byteOffset, rs.byteLength);
-    for (let i = 0; i < 16; i++) {
-      const b = Number(input % 256);
-      v.setInt8(i, b);
-      input = input / 256;
-    }
-    return Buffer.concat([toBuffer('0x01'), rs]);
-  }
-  throw new Error('Input is Uunknown type');
-}
-
 function serialiseExtraData(extra_data: { [key: string]: any }) {
   const expected_struct = {
-    risk: serialise,
-    stop: serialise,
-    time: serialise,
-    type: serialise,
+    risk: (input: boolean) => (input ? trueCV() : falseCV()),
+    stop: uintCV,
+    time: uintCV,
+    type: uintCV,
   };
 
   let _list: Buffer[] = [];
 
   for (const [key, func] of Object.entries(expected_struct))
     if (key in extra_data) {
-      _list.push(serialise(key));
-      _list.push(func(extra_data[key]));
+      _list.push(serializeCV(bufferCV(Buffer.from(key, 'ascii'))));
+      _list.push(serializeCV(func(extra_data[key])));
     } else {
       throw new Error(`Extra data object missing '${key}' field`);
     }
@@ -63,7 +52,7 @@ function serialiseExtraData(extra_data: { [key: string]: any }) {
   return Buffer.concat(_list);
 }
 
-let extra_data;
+let extra_data: any;
 try {
   extra_data = JSON.parse(process.argv[2]);
 } catch (error) {
