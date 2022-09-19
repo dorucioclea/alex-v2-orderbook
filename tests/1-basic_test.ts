8cd4fd129c696ae70a4f6db294ac650d2eff1490
fiftyeightandeight <alexd@alexgo.io>
2022-09-19 11:54:25 +0800

    working

--- tests/1-basic_test.ts
+++ tests/1-basic_test.ts
@@ -303,7 +303,7 @@ Clarinet.test({
 });
 
 Clarinet.test({
-  name: 'Core: can hash perp orders',
+  name: 'Core - Perp: can hash orders',
   fn(chain: Chain, accounts: Map<string, Account>) {
     const sender = accounts.get('wallet_1')!;
 
@@ -336,5 +336,130 @@ Clarinet.test({
       response.result,
       '0xc1d513ea01093e5aef0005ae6e5b89451d3568fec646aa6968287b7e52574302',
     );
+  },
+});
+
+Clarinet.test({
+  name: 'Core - Perp: can validate matching orders (signatures)',
+  fn(chain: Chain, accounts: Map<string, Account>) {
+    const sender = accounts.get('wallet_1')!;
+
+    const results = prepareChainBasicTest(chain, accounts);
+    results.receipts.forEach((e: any) => {
+      e.result.expectOk();
+    });
+
+    const left_order = perpOrderToTupleCV({
+      sender: 1,
+      'sender-fee': 1e8,
+      maker: 2,
+      'maker-asset': 1,
+      'taker-asset': 2,
+      'maker-asset-data': 14e8,
+      'taker-asset-data': 1e8,
+      'maximum-fill': 100,
+      'expiration-height': 100,
+      salt: 1,
+      risk: false,
+      stop: 0,
+      timestamp: 1,
+      type: 0,
+      'linked-hash': '0x',
+    });
+
+    const left_linked = perpOrderToTupleCV({
+      sender: 1,
+      'sender-fee': 1e8,
+      maker: 2,
+      'maker-asset': 2,
+      'taker-asset': 1,
+      'maker-asset-data': 1e8,
+      'taker-asset-data': 13.3e8, // 5% down
+      'maximum-fill': 100,
+      'expiration-height': 100,
+      salt: 2,
+      risk: true,
+      stop: 13.65e8, // 2.5% down
+      timestamp: 1,
+      type: 0,
+      'linked-hash':
+        '0xd55293082781e8706ae5f43673a5332e063dc8d3846e9c54a78dfa6dadc69577',
+    });
+
+    const right_order = orderToTupleCV({
+      sender: 1,
+      'sender-fee': 1e8,
+      maker: 3,
+      'maker-asset': 2,
+      'taker-asset': 1,
+      'maker-asset-data': 1e8,
+      'taker-asset-data': 14e8,
+      'maximum-fill': 50,
+      'expiration-height': 100,
+      salt: 2,
+      risk: false,
+      stop: 0,
+      timestamp: 2,
+      type: 0,
+      'linked-hash': '0x',
+    });
+
+    const right_linked = orderToTupleCV({
+      sender: 1,
+      'sender-fee': 1e8,
+      maker: 3,
+      'maker-asset': 1,
+      'taker-asset': 2,
+      'maker-asset-data': 14.7e8,
+      'taker-asset-data': 1e8,
+      'maximum-fill': 50,
+      'expiration-height': 100,
+      salt: 3,
+      risk: true,
+      stop: 14.35e8,
+      timestamp: 2,
+      type: 0,
+      'linked-hash': '0x',
+    });
+    // console.log(left_order, right_order);
+
+    // yarn generate-perpetual-hash "{ \"sender\": 1, \"sender-fee\": 1e8, \"maker\": 2, \"maker-asset\": 1, \"taker-asset\": 2, \"maker-asset-data\": 14e8, \"taker-asset-data\": 1e8, \"maximum-fill\": 100, \"expiration-height\": 100, \"salt\": 1, \"risk\": false, \"stop\": 0, \"timestamp\": 1, \"type\": 0, \"linked-hash\": \"0x\" }"
+    const left_order_hash =
+      '0xd55293082781e8706ae5f43673a5332e063dc8d3846e9c54a78dfa6dadc69577';
+    // yarn generate-perpetual-hash "{ \"sender\": 1, \"sender-fee\": 1e8, \"maker\": 3, \"maker-asset\": 2, \"taker-asset\": 1, \"maker-asset-data\": 1e8, \"taker-asset-data\": 14e8, \"maximum-fill\": 50, \"expiration-height\": 100, \"salt\": 2, \"risk\": false, \"stop\": 0, \"timestamp\": 2, \"type\": 0, \"linked-hash\": \"0x\" }"
+    const right_order_hash =
+      '0x5d237553fb165c63f11782ee66d2779d6dd1a4b89f72aca7fd402c77d35954b6';
+
+    // yarn sign-order-hash 530d9f61984c888536871c6573073bdfc0058896dc1adfe9a6a10dfacadc209101 0xa891215d95196515149eb214c5812b5b9b0f102edc7a8dae3f30940dbed1bd70
+    const left_signature =
+      '0x51b195c240eae83f7c42438a673c2338105851ab11d0ac6ded878d14981e9b7479b1a62a18af10b34a21acd0392cec7cfbb3b8ea19b355a7268e2526bddd66f401';
+    // yarn sign-order-hash d655b2523bcd65e34889725c73064feb17ceb796831c0e111ba1a552b0f31b3901 0xad5faa9b4b22cd3a55e59b763e052674fb29c9334734f38146250239e25e15c6
+    const right_signature =
+      '0x5e3c2e1f8d414b3abef2891a9198fe278534a648e2e01d92af4c52b381cfe4b5756fd6a7e6111110bf6995463a73ab055863d9e131de34cb9a467c73918bb49600';
+
+    let response = chain.callReadOnlyFn(
+      contractNames.exchange,
+      'validate-match',
+      [
+        left_order,
+        right_order,
+        left_signature,
+        right_signature,
+        types.none(),
+        types.none(),
+        types.none(),
+      ],
+      sender.address,
+    );
+    let response_tuple = response.result.expectOk().expectTuple();
+    assertEquals(response_tuple, {
+      fillable: types.uint(50),
+      'left-order-fill': types.uint(0),
+      'left-order-hash': left_order_hash,
+      'right-order-fill': types.uint(0),
+      'right-order-hash': right_order_hash,
+      'left-order-make': types.uint(14e8),
+      'right-order-make': types.uint(1e8),
+    });
   },
 });
